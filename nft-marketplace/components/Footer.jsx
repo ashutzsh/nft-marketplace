9cbbef0866bed770a6e49e200c7265ef0a643868
Ashutosh Kumar <ashutoshkumar6328@gmail.com>
2022-10-28 16:42:06 +0530

    Footer Created

--- nft-marketplace/components/Footer.jsx
+++ nft-marketplace/components/Footer.jsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import { useTheme } from 'next-themes'; // This is going to give us information whether we currently have the light mode or dark mode turned on.
+import Image from 'next/image'; // This is the Next.js version of the <img> tag
 
-const Footer = () => (
-  <div>Footer</div>
+import images from '../assets'; // Importing images from assets folder
+import Button from './Button';
+
+// Creating Footer Links Component:
+const FooterLinks = ({ heading, items }) => (
+  <div className="flex-1 justify-start items-start">
+    <h3 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl mb-10">{heading}</h3>
+    {items.map((item, index) => (
+      <p key={index} className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-2 hover:text-nft-gray-3 my-3">{item}</p>
+    ))}
+  </div>
 );
 
+const Footer = () => {
+  const { theme } = useTheme();
+
+  return (
+    <footer className="flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16">
+      <div className="w-full minmd:w-4/5 flex flex-row md:flex-col sm:px-4 px-16">
+        <div className="flexStart flex-1 flex-col">
+          <div className="flexCenter cursor-pointer">
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-1g ml-1">KryptoArt</p>
+          </div>
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base mt-6">Get the Latest Updates</p>
+          <div className="flexBetween md:w-full minlg:w-557 w-357 mt-6 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounder-md">
+            <input type="email" placeholder="Your Email" className="h-full flex-1 w-full dark:bg-nft-black-2 bg-white px-4 rounded-md dark:text-white text-nft-black-1 font-normal text-xs minlg:text-lg outline-none" />
+            <div className="flex-initial">
+              <Button btnName="Email me" classStyles="rounded-md" />
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 flexBetweenStart flex-wrap ml-10 md:ml-0 md:mt-8">
+          <FooterLinks heading="KryptoArt" items={['Explore', 'How it Works', 'Contact Us']} />
+          <FooterLinks heading="Support" items={['Help Center', 'Terms of Service', 'Legal', 'Privacy Policy']} />
+        </div>
+
+      </div>
+
+      <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
+        <div className="flexBetween flex-row w-full minmd:w-4/5 sm:flex-col mt-7">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">KryptoArt, Inc. All Rights Reserved</p>
+          <div className="flex flex-row sm:mt-4">
+            {[images.instagram, images.twitter, images.telegram, images.discord].map((image, index) => (
+              <div className="mx-2 cursor-pointer" key={index}>
+                <Image
+                  src={image}
+                  objectFit="contain"
+                  width={24}
+                  height={24}
+                  alt="social"
+                  className={theme === 'light' && 'filter invert'}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <p className="-mb-20 pb-4 md:mt-10 sm:mt-10 xs:mt-10 font-source">Made with <span className="text-red-500">❤</span>  by Ashutosh</p>
+    </footer>
+
+  );
+};
 export default Footer;
